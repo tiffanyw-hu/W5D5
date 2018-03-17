47b00a1f90d941dc518d8fc89663a72472e6a2ba
Andrew Tae <tiffanyw.hu@gmail.com>
2018-03-16 18:22:34 -0700

    fixed bubble sort

--- absurdBubbleSort.js
+++ absurdBubbleSort.js
@@ -21,25 +21,33 @@ function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
 
   if ( i == (arr.length - 1)) {
   outerBubbleSortLoop(madeAnySwaps);
-  }
+}
 
   if ( i < arr.length - 1) {
     askIfGreaterThan( arr[i], arr[i+1], (isGreaterThan) => {
-      if ( isGreaterThan === false) {
+      if ( isGreaterThan === true) {
         temp = arr[i];
         arr[i] = arr[i+1];
         arr[i+1] = temp;
         madeAnySwaps = true;
-        innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
-      } else { continue };
-    }
-  })
+      }
+    innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
+  }
+  )}
 };
 
 function absurdBubbleSort(arr, sortCompletionCallback) {
-  if (madeAnySwaps === true) {
-    innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
-  } else {
-    sortCompletionCallback(arr);
-  }
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if (madeAnySwaps === true) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  };
+  outerBubbleSortLoop(true);
 };
+
+absurdBubbleSort([3, 7, 2], function (arr) {
+  console.log(JSON.stringify(arr));
+  reader.close();
+});
